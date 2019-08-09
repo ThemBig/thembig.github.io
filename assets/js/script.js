ccecbe0a13d93beb5801ba9934f854cd1eeb2e7d
thembig <malilfajar17@gmail.com>
2019-08-09 13:55:29 +0700

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,11 @@ let about, contact, education, hobby, contactItem;
   contact = `<div id="contact">
                 Call me in :
                 <ul>
-                  <li title="facebook"><img src="./assets/img/facebook-logo.png" alt="facebook">&nbsp;&nbsp;<span class="hide">Facebook</span></li>
-                  <li title="GitHub"><img src="./assets/img/github.png" alt="github">&nbsp;&nbsp;<span class="hide">GitHub</span></li>
-                  <li title="Instagram"><img src="./assets/img/insta.png" alt="instagram">&nbsp;&nbsp;<span class="hide">Instagram</span></li>
-                  <li title="Twitter"><img src="./assets/img/twitter.png" alt="twitter">&nbsp;&nbsp;<span class="hide">Twitter</span></li>
-                  <li title="WhatsApp"><img src="./assets/img/whatsapp-logo.png" alt="whatsapp">&nbsp;&nbsp;<span class="hide">WhatsApp</span></li>
+                  <li title="facebook"><a href="https://facebook.com/_thembig" target="_blank"><img src="./assets/img/facebook-logo.png" alt="facebook">&nbsp;&nbsp;<span class="hide">Facebook</span></a></li>
+                  <li title="GitHub"><a href="https://github.com/thembig" target="_blank"><img src="./assets/img/github.png" alt="github">&nbsp;&nbsp;<span class="hide">GitHub</span></a></li>
+                  <li title="Instagram"><a href="https://instagram.com/_thembig" target="_blank"><img src="./assets/img/insta.png" alt="instagram">&nbsp;&nbsp;<span class="hide">Instagram</span></a></li>
+                  <li title="Twitter"><a href="https://twitter.com/_thembig" target="_blank"><img src="./assets/img/twitter.png" alt="twitter">&nbsp;&nbsp;<span class="hide">Twitter</span></a></li>
+                  <li title="WhatsApp"><a href="tel:+6285878776936"><img src="./assets/img/whatsapp-logo.png" alt="whatsapp">&nbsp;&nbsp;<span class="hide">WhatsApp</span></a></li>
                 </ul>
               </div>`;
   education = `<div id="education">
@@ -34,107 +34,94 @@ let about, contact, education, hobby, contactItem;
             </ul>
           </div>`;
 
-  contactItem = function() {
-    //contact items
+  contactItem = function() { //contact items
     $('li[title="facebook"]').mouseenter(function() {
       $('li[title="facebook"] span').removeClass('hide');
       $('li[title="facebook"] span').addClass('show');
-
     });
 
     $('li[title="facebook"]').mouseleave(function() {
       $('li[title="facebook"] span').removeClass('show');
       $('li[title="facebook"] span').addClass('hide');
-
     });
 
     $('li[title="GitHub"]').mouseenter(function() {
       $('li[title="GitHub"] span').removeClass('hide');
       $('li[title="GitHub"] span').addClass('show');
-
     })
 
     $('li[title="GitHub"]').mouseleave(function() {
       $('li[title="GitHub"] span').removeClass('show');
       $('li[title="GitHub"] span').addClass('hide');
-
     })
 
     $('li[title="Instagram"]').mouseenter(function() {
       $('li[title="Instagram"] span').removeClass('hide');
       $('li[title="Instagram"] span').addClass('show');
-
     })
 
     $('li[title="Instagram"]').mouseleave(function() {
       $('li[title="Instagram"] span').removeClass('show');
       $('li[title="Instagram"] span').addClass('hide');
-
     })
 
     $('li[title="Twitter"]').mouseenter(function() {
       $('li[title="Twitter"] span').removeClass('hide');
       $('li[title="Twitter"] span').addClass('show');
-
     })
 
     $('li[title="Twitter"]').mouseleave(function() {
       $('li[title="Twitter"] span').removeClass('show');
       $('li[title="Twitter"] span').addClass('hide');
-
     })
 
     $('li[title="WhatsApp"]').mouseenter(function() {
       $('li[title="WhatsApp"] span').removeClass('hide');
       $('li[title="WhatsApp"] span').addClass('show');
-
     })
 
     $('li[title="WhatsApp"]').mouseleave(function() {
       $('li[title="WhatsApp"] span').removeClass('show');
       $('li[title="WhatsApp"] span').addClass('hide');
-
     })
   }
 
   fade = (content) => {
     $('.content').fadeOut(function() {
       $('.content').html(content);
-      $('.content').fadeIn()
-    })
-  }
+      $('.content').fadeIn();
+    });
+  };
+
 $(function() {
 
 
   $('.about').click(function() {
-    $('a').css("text-decoration", "none")
-    $('.about').css("text-deoration", "underline");
-    fade(about)
+    $('.active').removeClass('active');
+    $('.about').addClass('active');
+    fade(about);
   })
 
   $('.contact').click(function() {
-    $('a').css("text-decoration", "none")
-    $('.contact').css("text-deoration", "underline")
+    $('.active').removeClass('active');
+    $('.contact').addClass('active');
     $('.content').fadeOut(function() {
       $('.content').html(contact);
       $('.content').fadeIn(function() {
         contactItem();
-      })
-    })
-  })
+      });
+    });
+  });
 
   $('.education').click(function() {
-    $('a').css("text-decoration", "none")
-    $('.contact').css("text-deoration", "underline")
+    $('.active').removeClass('active');
+    $('.education').addClass('active');
     fade(education);
-  })
+  });
 
   $('.hobby').click(function() {
-    $('a').css("text-decoration", "none")
-    $('.contact').css("text-deoration", "underline")
+    $('.active').removeClass('active');
+    $('.hobby').addClass('active');
     fade(hobby);
-  })
-
-
-
+  });
 })
