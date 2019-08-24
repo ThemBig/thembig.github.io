e905edcdb789a56d390fab8e67fed723ed255810
thembig <malilfajar17@gmail.com>
2019-08-24 11:23:02 +0700

    up

--- assets/js/js2.js
+++ assets/js/js2.js
@@ -1,6 +1,26 @@
 const content = document.getElementsByClassName('content')[0];
 const about = document.getElementsByClassName('about')[0];
-
+let navbar = document.getElementsByClassName('navbar')[0];
 about.addEventListener('click', function() {
   content.scrollTop = 0;
 })
+
+// if (navbar.scrollTop == 0) {
+//   navbar.style.position = "fixed";
+// }
+
+window.onscroll = function() {myFunction()};
+window.onresize = function() {navbar.offset().top;
+  console.log('resize', sticky);
+
+}
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
