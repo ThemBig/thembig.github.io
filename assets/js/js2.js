cda5254e77fbfc7d3e9c633d020c2abeca0ba468
thembig <malilfajar17@gmail.com>
2019-08-26 10:59:16 +0700

    up

--- assets/js/js2.js
+++ assets/js/js2.js
@@ -4,23 +4,3 @@ let navbar = document.getElementsByClassName('navbar')[0];
 about.addEventListener('click', function() {
   content.scrollTop = 0;
 })
-
-// if (navbar.scrollTop == 0) {
-//   navbar.style.position = "fixed";
-// }
-
-window.onscroll = function() {myFunction()};
-window.onresize = function() {navbar.offset().top;
-  console.log('resize', sticky);
-
-}
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
