a120a0265c3e08b507dc606a0ff4db2151af216b
ThemBig <malilfajar17@gmail.com>
2019-08-30 05:25:49 +0700

    fix fix fix view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 const content = document.getElementsByClassName('content')[0];
 const about = document.getElementsByClassName('about')[0];
-let navbar = document.getElementsByClassName('navbar')[0];
 about.addEventListener('click', function() {
   content.scrollTop = 0;
 })
+
+const myBorn = 2002;
+let myAge = () => {
+  const dt = new Date();
+  return (dt.getFullYear() - 2002);
+}
+const age = document.getElementById('age');
+age.innerHTML = myAge();
